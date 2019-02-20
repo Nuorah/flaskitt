841857d1e876eaa4e46a5ff5cc5500b2289e8309
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-02-20 17:19:38 +0100

    some templates and database

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template, redirect, url_for
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
