0e6ee7c4bf97ead9528af84a8a8adfd4ddb39fc4
Haroun Moatassime <haroun.moatassime@gmail.com>
2019-02-21 15:03:01 +0100

    Simplest link aggregator possible, requirements

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, ValidationError, URL
+from app.models import User, Post
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField(
+		'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different username.')
+
+class SubmitForm(FlaskForm):
+	title = StringField("Title", validators=[DataRequired()])
+	url = StringField("Url", validators=[DataRequired(), URL()])
+	submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,18 @@
-from app import app
-from flask import render_template, redirect, url_for
-from app.forms import LoginForm
+from app import app, db
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_required, logout_user, current_user, login_user
+from werkzeug.urls import url_parse
+from app.forms import LoginForm, RegistrationForm, SubmitForm
+from app.models import User, Post
 
 @app.route('/')
 @app.route('/index')
+#@login_required
 def index():
-    return render_template('index.html')
+	posts = Post.query.all()
+	for post in posts:
+		print(post.title)
+	return render_template('index.html', title = 'Front page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -16,5 +23,39 @@ def login():
 			flash('Invalid username or password')
 			return redirect(url_for('login'))
 		login_user(user, remember=form.remember_me.data)
-		return redirect(url_for('index'))
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
 	return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Congratulations, you are now a registered user!')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
+
+@login_required
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+	form = SubmitForm()
+	if form.validate_on_submit():
+		post = Post(title=form.title.data, url = form.url.data, user_id = current_user.id)
+		db.session.add(post)
+		db.session.commit()
+		flash('Link submitted.')
+		return redirect(url_for('index'))
+	return render_template('submit.html', title='Submit', form=form)
